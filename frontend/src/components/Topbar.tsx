ec62847946c6fd0af0c860b9a4a9694fab74fb6c
colorfulparrot06 <pallavi.notify>
2025-10-04 21:55:39 -0500

    changed get started button to home button, routs to input page. there are two model pages, will come back to

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "../styles/Topbar.css";
+import { useNavigate } from "react-router-dom";
 
 const Topbar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="topbar">
       <div className="topbar-title">Tierra Heat Simulations</div>
-      <button className="topbar-button">Get Started</button>
+      <button className="topbar-button" onClick={() => navigate("/")}>
+        Home
+      </button>{" "}
     </div>
   );
 };
